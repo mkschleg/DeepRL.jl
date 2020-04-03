c19cb8c0038b36bb8927e1be0b6946a609939636
Matthew Schlegel <mkschleg@gmail.com>
2020-04-03 15:00:51 -0600

    Updated w/ Atari DQN settings for dopamine and revisiting ALE.

--- experiment/atari.jl
+++ experiment/atari.jl
@@ -10,63 +10,6 @@ using LinearAlgebra
 using BSON: @save
 # using Distributions
 
-flatten(x) = reshape(x, :, size(x, 4))
-
-function construct_agent(env)
-    # Replicate Dopamine's Results!
-    
-    γ=0.99
-    batch_size=32
-    buffer_size = 1000000
-    tn_update_freq= 8000
-    hist_length = 4
-    update_freq = 4
-    min_mem_size = 20000
-
-    learning_rate = 0.00025
-    momentum_term = 0.00
-    squared_grad_term = 0.95
-    min_grad_term = 1e-5
-
-    example_state = MinimalRLCore.get_state(env)
-
-    init_f = Flux.glorot_uniform
-    
-    model = Chain(
-        Conv((8,8), hist_length=>32, relu, stride=4, init=init_f),
-        Conv((4,4), 32=>64, relu, stride=2, init=init_f),
-        Conv((3,3), 64=>64, relu, stride=1, init=init_f),
-        flatten,
-        Dense(3136, 512, relu, initW=init_f),
-        Dense(512, length(get_actions(env)), identity, initW=init_f)) |> gpu
-
-    target_network  = deepcopy(model)
-
-
-    return DQNAgent(
-        model,
-        target_network,
-        QLearningHuberLoss(γ),
-        DeepRL.RMSPropTFCentered(learning_rate,
-                                 squared_grad_term,
-                                 momentum_term,
-                                 min_grad_term),
-        DeepRL.ϵGreedyDecay((1.0, 0.01), 250000, min_mem_size, get_actions(env)),
-        buffer_size,
-        hist_length,
-        example_state,
-        batch_size,
-        tn_update_freq,
-        update_freq,
-        min_mem_size;
-        hist_squeeze = Val{false}(),
-        state_preproc = DeepRL.image_manip_atari,
-        state_postproc = DeepRL.image_norm,
-        rew_transform = (r)->clamp(Float32(r), -1.0f0, 1.0f0),
-        device = Flux.use_cuda[] ? Val{:gpu}() : Val{:cpu}())
-end
-
-
 function main_experiment(seed,
                          num_frames,
                          save_loc,
@@ -87,12 +30,11 @@ function main_experiment(seed,
     end
 
     res_save_file = joinpath(save_loc, "results.bson")
-    
 
     Random.seed!(Random.GLOBAL_RNG, seed)
     env = DeepRL.RevisitingALEAtari(gamename, rand(UInt16))
+    agent = DeepRL.RevisitingALEDQNBaseline(env)
 
-    agent = construct_agent(env)
     total_rews = Array{Int,1}()
     steps = Array{Int,1}()
 
--- src/agent.jl
+++ src/agent.jl
@@ -2,3 +2,8 @@
 
 include("agent/DQN.jl")
 
+
+
+
+# Settings
+include("agent/settings/DQN.jl")
